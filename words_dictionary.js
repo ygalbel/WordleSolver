e989133b0b29fe49ec5d6aed64610d4e979f84e0
Ygal Bellaiche <ygal.bellaiche@sap.com>
2022-04-14 15:28:00 +0300

    add specificNot function

--- words_dictionary.js
+++ words_dictionary.js
@@ -35,19 +35,36 @@ class solution  {
     
         return this;
     }
+
+    specificNot(){
+        console.log(arguments);
+        for(var i=0; i <arguments.length; i++){
+            console.log(this.dict.length);
+            this.dict = this.dict.filter(a => 
+                a.indexOf(arguments[i].letter) != arguments[i].location);
+        }
+    
+        return this;
+    }
 }
 
 
 var u = new solution()
         .length(5)
-        .existingLetters( 'r',)
-        .nonExistingLetter('w','t','y','i','o','a','s','d',
-        'h','j','l','c','b','n','m')
-        .specific({
-           letter : 'u', location: 1 
-        },{
-            letter : 'e', location: 4 
-         });
+        .nonExistingLetter('r', 't','u','o','p','a','d','f',
+        'h','l','b')
+        .specific(
+            { letter : 'i', location: 1},
+            { letter : 'c', location: 3},
+            { letter : 'e', location: 4},
+            )
+            .specificNot(
+                { letter : 'c' , location: 1},
+                { letter : 'e' , location: 2},
+                { letter : 'n' , location: 4},
+                { letter : 'i' , location: 2})
+        .existingLetters( 'i','c', 'n')
+        
     
 
 
