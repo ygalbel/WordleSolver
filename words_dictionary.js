b175a31638870c64b5ec68273bcb9364466655c2
Ygal Bellaiche <ygal.bellaiche@sap.com>
2022-04-26 19:07:11 +0300

    Added proposed word

--- words_dictionary.js
+++ words_dictionary.js
@@ -1,70 +1,68 @@
-
-
-class solution  {
-    constructor(){
+class solution {
+    constructor() {
         var dict = require("./words_dictionary.json");
         var words = Object.keys(dict)
         this.dict = words;
     }
-    
-    existingLetters(){
-        for(var i=0; i <arguments.length; i++){
+
+    existingLetters() {
+        for (var i = 0; i < arguments.length; i++) {
             this.dict = this.dict.filter(a => a.indexOf(arguments[i]) > -1);
         }
-    
+
         return this;
     }
-    length(length){
+    length(length) {
         this.dict = this.dict.filter(a => a.length == length);
         return this;
     }
-    nonExistingLetter(){
-        for(var i=0; i <arguments.length; i++){
+    nonExistingLetter() {
+        for (var i = 0; i < arguments.length; i++) {
             this.dict = this.dict.filter(a => a.indexOf(arguments[i]) == -1);
         }
-    
+
         return this;
     }
-    specific(){
+    specific() {
         console.log(arguments);
-        for(var i=0; i <arguments.length; i++){
+        for (var i = 0; i < arguments.length; i++) {
             console.log(this.dict.length);
-            this.dict = this.dict.filter(a => 
+            this.dict = this.dict.filter(a =>
                 a.indexOf(arguments[i].letter) == arguments[i].location);
         }
-    
+
         return this;
     }
 
-    specificNot(){
+    specificNot() {
         console.log(arguments);
-        for(var i=0; i <arguments.length; i++){
+        for (var i = 0; i < arguments.length; i++) {
             console.log(this.dict.length);
-            this.dict = this.dict.filter(a => 
+            this.dict = this.dict.filter(a =>
                 a.indexOf(arguments[i].letter) != arguments[i].location);
         }
-    
+
         return this;
+    }
+    proposedWord() {
+        return this.dict.map(a => {
+                var obj = {
+                    "word": a,
+                    "differentLetters": new Set(a.split('')).size
+                };
+                return obj;
+            })
+            .reduce((a, b) => a.differentLetters > b.differentLetters ? a : b).word
     }
 }
 
 
 var u = new solution()
-        .length(5)
-        .nonExistingLetter('r','u','a','d','b' ,'m', 'c')
-         .specific(
-             { letter : 's', location: 3},
-             { letter : 't', location: 4}
-             )
-             .specificNot(
-                 { letter : 'e' , location: 2},
-                 { letter : 't' , location: 0},
-                 { letter : 'h' , location: 1}
-                 )
-        .existingLetters( 'e','t', 'h')
-        
-    
-
-
+    .length(5)
+    .nonExistingLetter('r', 'u', 'a', 'd', 'b', 'm', 'c')
+    .specific(
+        {letter: 's', location: 3}, 
+        {letter: 't', location: 4 })
 
 console.log(u.dict);
+console.log(u.proposedWord());
